029348bc354e852c93279ff63f5b52380b568e3a
dLeczycki <danielleczycki@gmail.com>
2023-04-27 08:14:40 +0200

    Fix ESLint and prettier config for endOfLine problem and react-in-jsx-scope rule

--- todo-fe/.eslintrc.cjs
+++ todo-fe/.eslintrc.cjs
@@ -22,5 +22,6 @@ module.exports = {
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'object-curly-spacing': ['error', 'always'],
+    'react/react-in-jsx-scope': 'off',
   },
 };
